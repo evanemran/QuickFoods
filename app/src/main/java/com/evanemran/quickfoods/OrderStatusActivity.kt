f84cad08248d8ba576af1a3a7170474c5aebb7b5
Emran Khandaker Evan <evanemran007@gmail.com>
2022-08-08 20:37:58 +0600

    status bar colors updated

--- app/src/main/java/com/evanemran/quickfoods/OrderStatusActivity.kt
+++ app/src/main/java/com/evanemran/quickfoods/OrderStatusActivity.kt
@@ -1,11 +1,14 @@
 package com.evanemran.quickfoods
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class OrderStatusActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_status)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
     }
 }
